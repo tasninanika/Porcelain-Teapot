2b318811d231e34cac2f906dbd5b1e4c55da5535
anikatasnin <jarintasnin09@gmail.com>
2025-07-21 14:49:14 +0600

    show the tea details

--- src/components/TeaDetails.jsx
+++ src/components/TeaDetails.jsx
@@ -2,7 +2,7 @@ import { HiMiniArrowLeftStartOnRectangle } from "react-icons/hi2";
 import { Link, useLoaderData } from "react-router-dom";
 
 const TeaDetails = () => {
-  const singleCoffee = useLoaderData();
+  const singleTea = useLoaderData();
   return (
     <div className="w-[70%] mx-auto my-16">
       <Link className="title text-2xl flex items-center my-3" to="/">
@@ -11,35 +11,34 @@ const TeaDetails = () => {
       <div className="bg-[#F4F3F0] p-11">
         <div className="lg:flex items-center">
           <div className="w-full">
-            <img src={singleCoffee?.photo} className="lg:w-[350px]" alt="" />
+            <img src={singleTea?.photo} className="lg:w-[350px]" alt="" />
           </div>
           <div className="w-full">
             <h1 className="title text-2xl mb-3 text-[#331A15]">Niceties</h1>
             <p className="text-[14px] mt-1">
-              <span className="font-semibold">Name: </span> {singleCoffee?.name}
+              <span className="font-semibold">Name: </span> {singleTea?.name}
             </p>
             <p className="text-[14px] mt-1">
-              <span className="font-semibold">Chef: </span> {singleCoffee?.chef}
+              <span className="font-semibold">Chef: </span> {singleTea?.chef}
             </p>
             <p className="text-[14px] mt-1">
               <span className="font-semibold">Supplier: </span>{" "}
-              {singleCoffee?.supplier}
+              {singleTea?.supplier}
             </p>
             <p className="text-[14px] mt-1">
               <span className="font-semibold">Category: </span>{" "}
-              {singleCoffee?.category}
+              {singleTea?.category}
             </p>
             <p className="text-[14px] mt-1">
               <span className="font-semibold">Details: </span>{" "}
-              {singleCoffee?.details}
+              {singleTea?.details}
             </p>
             <p className="text-[14px] mt-1">
-              <span className="font-semibold">Taste: </span>{" "}
-              {singleCoffee?.taste}
+              <span className="font-semibold">Taste: </span> {singleTea?.taste}
             </p>
             <p className="text-[14px] mt-1">
-              <span className="font-semibold">Price: </span>{" "}
-              {singleCoffee?.price} BDT
+              <span className="font-semibold">Price: </span> {singleTea?.price}{" "}
+              BDT
             </p>
           </div>
         </div>
