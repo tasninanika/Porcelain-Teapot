83995daa47a879e6d3d37c590d5ac65086d745d0
anikatasnin <jarintasnin09@gmail.com>
2025-07-21 13:00:53 +0600

    setup route

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,39 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import Main from "./components/Main";
+import Home from "./components/Home";
+import AddTea from "./components/AddTea";
+import TeaDetails from "./components/TeaDetails";
+import UpdateTea from "./components/UpdateTea";
+import ErrorPage from "./components/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/addTea",
+        element: <AddTea></AddTea>,
+      },
+      {
+        path: "/teas/:id",
+        element: <TeaDetails></TeaDetails>,
+      },
+      {
+        path: "/updateTea/:id",
+        element: <UpdateTea></UpdateTea>,
+      },
+      {
+        path: "*",
+        element: <ErrorPage></ErrorPage>,
+      },
+    ],
   },
 ]);
 
